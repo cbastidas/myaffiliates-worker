092c748697747b6f392ba5897205cb0d7a3eec0c
Christian Bastidas <cris931201@gmail.com>
2026-01-26 15:23:26 +0100

    fixing the port number

--- server.js
+++ server.js
@@ -92,5 +92,5 @@ app.post("/job", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Worker listening on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => console.log(`Worker listening on port ${PORT}`));
